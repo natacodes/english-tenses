9fab817570ee8dff77c23e967ecbc473ea7bb9bb
Natalia <sinelnikova.n@gmail.com>
2014-03-02 22:38:00 +0100

    Add event on esc key

--- js/impress.js
+++ js/impress.js
@@ -257,23 +257,32 @@
     // EVENTS
     
     document.addEventListener("keydown", function ( event ) {
-        if ( event.keyCode == 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+        if ( event.keyCode == 9 || event.keyCode == 27 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
             var next = active;
             switch( event.keyCode ) {
                 case 33: ; // pg up
-                case 37: ; // left
-                case 38:   // up
+                case 37:   // left
                          next = steps.indexOf( active ) - 1;
+                         next = next >= 0 ? steps[ next ] : steps[ steps.length-1 ];
+                         break;
+                case 38:   // up
+                         next = steps.indexOf( active ) - 3;
                          next = next >= 0 ? steps[ next ] : steps[ steps.length-1 ];
                          break;
                 case 9:  ; // tab
                 case 32: ; // space
                 case 34: ; // pg down
-                case 39: ; // right
-                case 40:   // down
+                case 39:   // right
                          next = steps.indexOf( active ) + 1;
+                         next = next >= 0 ? steps[ next ] : steps[ steps.length-1 ];
+                         break;
+                case 40:   // down
+                         next = steps.indexOf( active ) + 3;
                          next = next < steps.length ? steps[ next ] : steps[ 0 ];
-                         break; 
+                         break;
+                case 27:   // esc
+                         next = steps[ 0 ];
+                         break;
             }
             
             select(next);
